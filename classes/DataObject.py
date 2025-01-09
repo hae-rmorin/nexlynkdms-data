44ddc4c843ee20ed0bfac06db18c2d9ed847a619
rmorin <rmorin@haemonetics.com>
2025-01-09 16:38:44 -0700

    commiting visit related code

--- classes/DataObject.py
+++ classes/DataObject.py
@@ -3,14 +3,14 @@ class DataObject:
     def __init__(self):
         self.site_id = None
 
-        self.new_donor = True
+        self.new_donor = None
 
         #used in new_donor
         self.donor_id = None
         self.donor_birth_date = None
         self.donor_gender_ind = None
         self.donor_land_ind = None
-        self.donor_program_code_id = None
+        self.donor_program_id = None
         self.donor_collection_type_code = None
         self.donor_number = None
         self.donor_first_name = None
